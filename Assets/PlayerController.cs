79dd40ae20f404400f569e34b483dac2317fbb3b
Md. Akif Zaman <akifzaman4040@gmail.com>
2023-07-31 21:08:17 +0600

    Character Animation added

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float moveSpeed = 3f;
+	public Animator PlayerAnimator;
+	private void Update()
+	{
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+
+		PlayerAnimator.SetFloat("HorizontalInput", horizontalInput);
+		PlayerAnimator.SetFloat("VerticalInput", verticalInput);
+
+		Vector3 movement = new Vector3(horizontalInput, verticalInput, 0f);
+
+		movement = movement.normalized;
+
+		transform.position += movement * moveSpeed * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/Camera/CameraFollowPlayer.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+	public GameObject Player;
+    
+    private void Update()
+    {
+        transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z - 10);
+    }
+}
