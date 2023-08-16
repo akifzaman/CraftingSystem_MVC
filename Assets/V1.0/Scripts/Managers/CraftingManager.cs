d9b75b9cbc2bead212dcd5d100b0a0ab6e1349c8
Md. Akif Zaman <akifzaman4040@gmail.com>
2023-08-16 23:41:09 +0600

    Alternative crafting logic has been added
    Now crafting can be done without string matching/without maintaining the order of choosing the item from the inventory.

--- Assets/V1.0/Scripts/Managers/CraftingManager.cs
+++ Assets/V1.0/Scripts/Managers/CraftingManager.cs
@@ -2,64 +2,174 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
 public class CraftingManager : MonoBehaviour
 {
-	public static CraftingManager instance;
+    public static CraftingManager instance;
 
-	public int CraftingItemSlotCount;
-	public int ResultItemIndex;
-	public List<string> CraftingRecipes = new List<string>();
-	public List<CraftingItem> Items = new List<CraftingItem>();
-	public List<CraftedItem> ResultItems = new List<CraftedItem>();
-	public string currentCraftingRecipe;
-	public Button CraftButton;
+    public int CraftingItemSlotCount;
+    public int ResultItemIndex;
+    public List<string> CraftingRecipes = new List<string>();
+    public List<CraftingItem> Items = new List<CraftingItem>();
+    public List<CraftedItem> ResultItems = new List<CraftedItem>();
+    public string currentCraftingRecipe;
+    public Button CraftButton;
 
-	#region Singleton
-	public void Awake()
-	{
-		if (instance == null) instance = this;
-		else Destroy(this.gameObject);
-	}
+    #region Singleton
+    public void Awake()
+    {
+        if (instance == null) instance = this;
+        else Destroy(this.gameObject);
+    }
     #endregion
 
     public void Start()
     {
-		CraftButton.onClick.AddListener(OnCraftButtonClicked);
+        CraftButton.onClick.AddListener(OnCraftButtonClicked);
     }
-	public void OnCraftButtonClicked()
-	{
-		CraftedItem craftedItem = Craft();
-		if (craftedItem == null) return;
-		InventoryManager.instance.inventory.CraftedItems.Add(craftedItem);
-		UIManager.Instance.OnItemCrafted(craftedItem);
-	}
-	public CraftedItem Craft()
-	{
-		if (!IsRecipeValid())
-		{
-			UIManager.Instance.ShowAlert("Invalid Recipe!");
-			return null;
-		}
-		UIManager.Instance.DestroyCraftingItems();
+    public void OnCraftButtonClicked()
+    {
+        CraftedItem craftedItem = Craft();
+        if (craftedItem == null) return;
+        InventoryManager.instance.inventory.CraftedItems.Add(craftedItem);
+        UIManager.Instance.OnItemCrafted(craftedItem);
+    }
+    public CraftedItem Craft()
+    {
+        if (!IsRecipeValid())
+        {
+            UIManager.Instance.ShowAlert("Invalid Recipe!");
+            return null;
+        }
+        UIManager.Instance.DestroyCraftingItems();
         UIManager.Instance.ShowAlert(ResultItems[ResultItemIndex].itemName + " Crafted!");
         Items.Clear();
         return ResultItems[ResultItemIndex];
     }
-	public bool IsRecipeValid()
-	{
-		if (Items.Count < CraftingItemSlotCount) return false;
-		else
-		{
-			currentCraftingRecipe = "";
-			ResultItemIndex = -1;
+    public bool IsRecipeValid()
+    {
+        if (Items.Count < CraftingItemSlotCount) return false;
+        else
+        {
+            currentCraftingRecipe = "";
+            ResultItemIndex = -1;
             foreach (CraftingItem item in Items) currentCraftingRecipe += item.itemName;
-			foreach (var recipe in CraftingRecipes)
-			{
-				ResultItemIndex++;
-				if (recipe == currentCraftingRecipe) return true;
-			}
-		}
+            foreach (var recipe in CraftingRecipes)
+            {
+                ResultItemIndex++;
+                if (recipe == currentCraftingRecipe) return true;
+            }
+        }
         return false;
     }
 }
+
+
+
+
+
+
+
+//Second approach for crafting
+//Without ordering/string match crafting, following this you can craft placing the items in any order
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using UnityEngine;
+//using UnityEngine.UI;
+
+//[Serializable]
+//public class CraftingRecipe
+//{
+//    public List<string> ingredients = new List<string>(4);
+//}
+
+//public class CraftingManager : MonoBehaviour
+//{
+//    public static CraftingManager instance;
+
+//    public int CraftingItemSlotCount;
+//    public int ResultItemIndex;
+//    public List<CraftingRecipe> CraftingRecipes = new List<CraftingRecipe>();
+//    public List<CraftingItem> Items = new List<CraftingItem>();
+//    public List<CraftedItem> ResultItems = new List<CraftedItem>();
+//    public string currentCraftingRecipe;
+//    public Button CraftButton;
+
+//    #region Singleton
+//    public void Awake()
+//    {
+//        if (instance == null) instance = this;
+//        else Destroy(this.gameObject);
+//    }
+//    #endregion
+//    public void Start()
+//    {
+//        CraftButton.onClick.AddListener(OnCraftButtonClicked);
+//    }
+//    public void OnCraftButtonClicked()
+//    {
+//        CraftedItem craftedItem = Craft();
+//        if (craftedItem == null) return;
+//        InventoryManager.instance.inventory.CraftedItems.Add(craftedItem);
+//        UIManager.Instance.OnItemCrafted(craftedItem);
+//    }
+
+//    public CraftedItem Craft()
+//    {
+//        if (!IsRecipeValid())
+//        {
+//            UIManager.Instance.ShowAlert("Invalid Recipe!");
+//            return null;
+//        }
+//        UIManager.Instance.DestroyCraftingItems();
+//        UIManager.Instance.ShowAlert(ResultItems[ResultItemIndex].itemName + " Crafted!");
+//        Items.Clear();
+//        return ResultItems[ResultItemIndex];
+//    }
+
+//    public bool IsRecipeValid()
+//    {
+//        if (Items.Count < CraftingItemSlotCount) return false;
+//        else
+//        {
+//            List<string> ingredientNames = new List<string>();
+//            foreach (CraftingItem item in Items)
+//            {
+//                ingredientNames.Add(item.itemName);
+//            }
+//            ingredientNames.Sort();
+
+//            foreach (var recipe in CraftingRecipes)
+//            {
+//                List<string> sortedRecipe = new List<string>(recipe.ingredients);
+//                sortedRecipe.Sort();
+
+//                if (IsListsEqual(ingredientNames, sortedRecipe))
+//                {
+//                    ResultItemIndex = CraftingRecipes.IndexOf(recipe);
+//                    return true;
+//                }
+//            }
+//        }
+
+//        return false;
+//    }
+
+//    private bool IsListsEqual(List<string> list1, List<string> list2)
+//    {
+//        if (list1.Count != list2.Count)
+//        {
+//            return false;
+//        }
+
+//        for (int i = 0; i < list1.Count; i++)
+//        {
+//            if (list1[i] != list2[i])
+//            {
+//                return false;
+//            }
+//        }
+
+//        return true;
+//    }
+//}
